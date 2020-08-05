9db5de3eb2f65dd2dbf2c80ceac4e89666c1ab7f
sanche00 <sanche00@naver.com>
2020-08-05 16:01:09 +0900

    first commit

---
+++ rest-doc/src/test/java/com/jwk/spring/demo/RestDocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jwk.spring.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestDocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
