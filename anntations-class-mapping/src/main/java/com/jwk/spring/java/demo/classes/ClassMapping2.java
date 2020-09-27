85a5b0d60177b1cbd15c164fe44dd87655f4b6ce
sanche00 <sanche00@naver.com>
2020-09-27 23:51:56 +0900

    list 기능 변경

--- anntations-class-mapping/src/main/java/com/jwk/spring/java/demo/classes/ClassMapping2.java
+++ anntations-class-mapping/src/main/java/com/jwk/spring/java/demo/classes/ClassMapping2.java
@@ -25,7 +25,7 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @Qualifier("ClassMapping")
 @Component
-public class ClassMapping implements Mapping {
+public class ClassMapping2 implements Mapping {
 
 	@Autowired
 	private BeanFactory beanFactory;
